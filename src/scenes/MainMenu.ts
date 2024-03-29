096f6a66d78f61122b92a99edc423624e0109cfc
Matthew Fotzler <mfotzler@gmail.com>
2024-03-29 02:03:27 -0500

    Add button text and clean up menu code

--- src/scenes/MainMenu.ts
+++ src/scenes/MainMenu.ts
@@ -1,8 +1,7 @@
 import * as Phaser from 'phaser';
 import MainScene from './MainScene';
-export default class MainMenu extends Phaser.Scene {
-    button: Phaser.GameObjects.NineSlice;
 
+export default class MainMenu extends Phaser.Scene {
     constructor() {
         super('GameScene');
     }
@@ -15,21 +14,29 @@ export default class MainMenu extends Phaser.Scene {
     create():void {
         this.addTitle();
         this.addPlayButton();
-        this.add.bitmapText(this.game.renderer.width/2, 250, 'rubik', 'a game by bugvevo, slowback1, and mafcho')
-            .setOrigin(0.5, 0.5);
     }
 
     private addTitle() {
         this.add.image(this.game.renderer.width / 2, 150, 'textures', 'title');
+        this.add.bitmapText(this.game.renderer.width/2, 250, 'rubik', 'a game by bugvevo, slowback1, and mafcho')
+            .setOrigin(0.5, 0.5);
     }
 
     private addPlayButton() {
-        this.button = this.add.nineslice(0, 0, 'textures', 'menu-button', 320, 64, 20, 20);
-        this.button.setPosition(this.game.renderer.width / 2, this.game.renderer.height / 2);
-        this.button.setInteractive();
-        this.button.on('pointerdown', () => {
+        this.addCenteredButton(400, 'Play', () => {
             this.scene.start(new MainScene());
         });
+    }
+
+    private addCenteredButton(y: number, text: string, onClick: () => void):Phaser.GameObjects.NineSlice {
+        const button = this.add.nineslice(0, 0, 'textures', 'menu-button', 420, 80, 20, 20, 20, 20);
+        button.setPosition(this.game.renderer.width / 2, y);
+        button.setInteractive();
+        button.on('pointerdown', onClick);
+        this.add.bitmapText(this.game.renderer.width/2, y, 'rubik', text)
+            .setOrigin(0.5, 0.5);
+
+        return button;
     }
 
     update(time: number, delta: number):void {
