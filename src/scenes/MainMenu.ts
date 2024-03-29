ff760ddbf4d1885d1516e0d90ed2d5e05176b1ba
Matthew Fotzler <mfotzler@gmail.com>
2024-03-29 01:03:39 -0500

    Add main menu scene and set up texture atlas for buttons

---
+++ src/scenes/MainMenu.ts
@@ -0,0 +1,26 @@
+import * as Phaser from 'phaser';
+import MainScene from './MainScene';
+export default class MainMenu extends Phaser.Scene {
+    button: Phaser.GameObjects.NineSlice;
+
+    constructor() {
+        super('GameScene');
+    }
+
+    preload():void {
+        this.load.atlas('textures', 'assets/texture.png', 'assets/texture.json');
+    }
+
+    create():void {
+        this.button = this.add.nineslice(0, 0, 'textures', 'menu-button', 320, 64, 20, 20);
+        this.button.setPosition(this.game.renderer.width / 2, this.game.renderer.height / 2);
+        this.button.setInteractive();
+        this.button.on('pointerdown', () => {
+            this.scene.start(new MainScene());
+        });
+
+    }
+
+    update(time: number, delta: number):void {
+    }
+}
