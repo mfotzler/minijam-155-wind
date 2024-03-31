16610c0611a3125313a1be97b1d0c324263839ad
Andrew Wobeck <dwobeck@gmail.com>
2024-03-31 12:20:13 -0500

    Update Vite config so that it will use relative paths for the js files instead of absolute (PLZZZZZZZZZ WORK)

--- vite.config.ts
+++ vite.config.ts
@@ -2,23 +2,26 @@ import { defineConfig } from 'vitest/config';
 import replace from '@rollup/plugin-replace';
 
 export default defineConfig({
-  build: {
-    rollupOptions: {
-      plugins: [
-        //  Toggle the booleans here to enable / disable Phaser 3 features:
-        replace({
-          'typeof CANVAS_RENDERER': "'true'",
-          'typeof WEBGL_RENDERER': "'true'",
-          'typeof EXPERIMENTAL': "'true'",
-          'typeof PLUGIN_CAMERA3D': "'false'",
-          'typeof PLUGIN_FBINSTANT': "'false'",
-          'typeof FEATURE_SOUND': "'true'"
-        }) as any
-      ]
-    }
-  },
-    test: {
-      include: ['src/**/*.{test,spec}.{js,ts}'],
-      globals: true,
-  }
+	base: '',
+	build: {
+		rollupOptions: {
+			plugins: [
+				//  Toggle the booleans here to enable / disable Phaser 3 features:
+				replace({
+					'typeof CANVAS_RENDERER': "'true'",
+					'typeof WEBGL_RENDERER': "'true'",
+					'typeof EXPERIMENTAL': "'true'",
+					'typeof PLUGIN_CAMERA3D': "'false'",
+					'typeof PLUGIN_FBINSTANT': "'false'",
+					'typeof FEATURE_SOUND': "'true'"
+				}) as any
+			],
+			makeAbsoluteExternalsRelative: true
+		},
+		emptyOutDir: true
+	},
+	test: {
+		include: ['src/**/*.{test,spec}.{js,ts}'],
+		globals: true
+	}
 });
