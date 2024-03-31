8ab585f617c93fceb81448539bb8b4fabe28263f
Andrew Wobeck <dwobeck@gmail.com>
2024-03-30 22:39:22 -0500

    make a dynamic coin ball size thingy

--- src/entities/CoinBall.ts
+++ src/entities/CoinBall.ts
@@ -4,11 +4,26 @@ import Vector2 = Phaser.Math.Vector2;
 import Arc = Phaser.GameObjects.Arc;
 import MessageBus from '../messageBus/MessageBus';
 import { Messages } from '../messageBus/Messages';
+import Sprite = Phaser.GameObjects.Sprite;
+import Body = Phaser.Physics.Arcade.Body;
+
+const BALL_SIZE_THRESHOLDS = {
+	small: 0,
+	medium: 1.5,
+	large: 2.0
+};
+
+const PHYSICS_BODY_SIZE = {
+	small: 50 / 2,
+	medium: 100 / 2,
+	large: 150 / 2
+};
 
 export default class CoinBall extends Container {
 	private static hasCreatedAnimations = false;
 	private readonly initialX: number;
 	private readonly initialY: number;
+	ballSprite: Sprite;
 	constructor(
 		scene: Phaser.Scene,
 		x: number,
@@ -21,35 +36,56 @@ export default class CoinBall extends Container {
 		this.initialX = x;
 
 		if (!CoinBall.hasCreatedAnimations) {
-			scene.anims.create({
-				key: 'roll',
-				frames: scene.anims.generateFrameNames('textures', {
-					prefix: 'coin-ball',
-					start: 1,
-					end: 3,
-					zeroPad: 2
-				}),
-				frameRate: 3,
-				repeat: -1
-			});
+			this.createBallAnimal(scene, 's');
+			this.createBallAnimal(scene, 'm');
+			this.createBallAnimal(scene, 'l');
 			CoinBall.hasCreatedAnimations = true;
 		}
 
 		this.initializeMessageBus();
-		const ballSprite = scene.add.sprite(0, 0, 'textures').play('roll');
-		this.add(ballSprite);
+		this.ballSprite = scene.add.sprite(0, 0, 'textures').play('roll-s');
+		this.add(this.ballSprite);
 	}
+
+	private createBallAnimal(scene: Phaser.Scene, size: string) {
+		scene.anims.create({
+			key: `roll-${size}`,
+			frames: scene.anims.generateFrameNames('textures', {
+				prefix: `coin-ball-${size}`,
+				start: 1,
+				end: 3,
+				zeroPad: 2
+			}),
+			frameRate: 3,
+			repeat: -1
+		});
+	}
+
+	ballScale: number = 1;
 
 	private initializeMessageBus() {
 		MessageBus.subscribe<number>(Messages.BallScale, (value) => {
-			this.scale = value ?? 1;
-			this.body.mass = value ?? 1;
+			this.ballScale = value ?? 1;
 		});
 	}
 
 	public update() {
 		this.moveBall();
 		this.decelerateBall();
+		this.setBallSize();
+	}
+
+	public setBallSize() {
+		if (this.ballScale > BALL_SIZE_THRESHOLDS.large) {
+			this.ballSprite.play('roll-l');
+			(this.body as any as Body).setCircle(PHYSICS_BODY_SIZE.large, -75, -75);
+		} else if (this.ballScale > BALL_SIZE_THRESHOLDS.medium) {
+			this.ballSprite.play('roll-m');
+			(this.body as any as Body).setCircle(PHYSICS_BODY_SIZE.medium, -50, -50);
+		} else if (this.ballScale > BALL_SIZE_THRESHOLDS.small) {
+			this.ballSprite.play('roll-s');
+			(this.body as any as Body).setCircle(PHYSICS_BODY_SIZE.small, -25, -25);
+		}
 	}
 
 	public resetBall() {
